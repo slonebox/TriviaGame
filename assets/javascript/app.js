90f32135eaeee1a9386b153ee91503fc96aaed1f
William Mooney Sloneker <wmsloneker@gmail.com>
2019-08-15 18:56:34 -0400

    Refined data structure for questions and answers; began writing function to display question and answer text to HTML

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,50 +5,137 @@ var score = correctAnswers/incorrectAnswers;
 
 //"Super Object" to contain all trivia questions, correct answers, and incorrect options
 var questionsArray = [
-    {
-        //QUESTION 0
+    {   //QUESTION 0
         questionText: 'The name "hippopotamus" is a combination of Greek words meaning...',
-        incorrect1:	"Water Bull",
-        correct: "River Horse",
-        incorrect2:	"Aqua Cow",
-        incorrect3:	"Sea Donkey"
+        answers: {
+            option1: {
+                optionText: "Water Bull",
+                correct: false,
+            },
+            option2: {
+                optionText: "River Horse",
+                correct: true,
+            },
+            option3: { 
+                optionText: "Creek Cow",
+                correct: false,
+            },
+            option4: {
+                optionText: "Wet Donkey",
+                correct: false,
+            }
+        }
     },
-    {
-        //QUESTION 1
+    {//QUESTION 1
         questionText: "What is the average lifespan of a hippopotamus?",
-        incorrect1:	"20 years",
-        incorrect2: "30 years",
-        correct: "40 years",
-        incorrect3:	"50 years"
+        answers: {
+            option1: {
+                optionText: "20 years",
+                correct: false,
+            },
+            option2: {
+                optionText: "30 years",
+                correct: false,
+            },
+            option3: { 
+                optionText: "40 years",
+                correct: true,
+            },
+            option4: {
+                optionText: "50 years",
+                correct: false,
+            }
+        }
+
+
     },
-    {
-        //QUESTION 2
+    {//QUESTION 2
         questionText: "How much time does a hippopotamus spend submerged in water each day?",
-        incorrect1:	"8 hours",
-        incorrect2: "12 hours",
-        correct: "16 hours",
-        incorrect3:	"20 hours"
+        answers: {
+            option1: {
+                optionText: "8 hours",
+                correct: false,
+            },
+            option2: {
+                optionText: "12 hours",
+                correct: false,
+            },
+            option3: { 
+                optionText: "16 hours",
+                correct: true,
+            },
+            option4: {
+                optionText: "20 hours",
+                correct: false,
+            }
+        }
+
     },
-    {
-        //QUESTION 3
+    {//QUESTION 3
         questionText: "How long can a hippopotamus hold its breath underwater?",
-        correct:	"5 minutes",
-        incorrect1: "10 minutes",
-        incorrect2: "15 minutes",
-        incorrect3:	"20 minutes"
+        answers: {
+            option1: {
+                optionText: "5 minutes",
+                correct: true,
+            },
+            option2: {
+                optionText: "10 minutes",
+                correct: false,
+            },
+            option3: { 
+                optionText: "15 minutes",
+                correct: false,
+            },
+            option4: {
+                optionText: "20 minutes",
+                correct: false,
+            }
+        }
     },
-    {
-        //QUESTION 4
+    {//QUESTION 4
         questionText: "What term best describes the diet of a hippopotamus?",
-        correct: "Herbivorous",
-        incorrect1: "Carnivorous",
-        incorrect2: "Omnivorous",
-        incorrect3:	"Paleo"
+        answers: {
+            option1: {
+                optionText: "Herbivorous",
+                correct: true,
+            },
+            option2: {
+                optionText: "Carnivorous",
+                correct: false,
+            },
+            option3: { 
+                optionText: "Omnivorous",
+                correct: false,
+            },
+            option4: {
+                optionText: "Paleo",
+                correct: false,
+            }
+        }
     }
 ];
+
+//Testing Data Structure
 var x = 1;
 
-console.log(questionsArray[1].questionText);
-console.log(questionsArray[1].correct);
+console.log(questionsArray[x].questionText);
+console.log(questionsArray[x].answers.option1.optionText);
+console.log(questionsArray[x].answers.option1.correct);
 
-//Function to begin timed sequence of cards 
+//On-click event to begin the game
+$("#start-button").on("click"); {
+    //runGame();
+    };
+
+//Function that starts the timer and begins iterating through the questions
+$(document).ready(function runGame() {
+    for (var i = 0; i < questionsArray.length; i++) {
+        $("#game-area").html("");
+        $("#game-area").append("<div>").text(questionsArray[i].questionText);
+        $("#game-area").append("<button>").text(questionsArray[i].answers.option1.optionText);
+        $("#game-area").append("<button>").text(questionsArray[i].answers.option2.optionText);
+        $("#game-area").append("<button>").text(questionsArray[i].answers.option3.optionText);
+        $("#game-area").append("<button>").text(questionsArray[i].answers.option4.optionText);
+        //add timer
+    };
+});
